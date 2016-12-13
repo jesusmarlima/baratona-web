115d5cab5b6bcd423b0a1e67f007cfa84982c5f2
Jesusmar Lima <jesusmarlima@gmail.com>
2016-12-13 17:11:32 -0500

    add login/logout

--- app/common/navigator.component.jsx
+++ app/common/navigator.component.jsx
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 import {Navbar,NavItem} from 'react-materialize';
+import {browserHistory} from "react-router";
+import Auth from '../session/Auth.js'
 
 class Navigator extends Component {
+
+    logout(ev){
+        ev.preventDefault();
+        Auth.deauthenticateUser();
+        browserHistory.push("/login")
+    }
+
     render() {
         return (
             <div>
-                <Navbar brand='Baratona' right>
+                <Navbar className='purple darken-4' brand='Baratona' right>
                     <NavItem href='/login'>Login</NavItem>
-                    <NavItem href='components.html'>Components</NavItem>
+                    <NavItem href='#' onClick={this.logout.bind(this)}>Logout</NavItem>
                 </Navbar>
                 <div className="container">
                     {this.props.children}
--- app/session/login.component.jsx
+++ app/session/login.component.jsx
@@ -1,7 +1,31 @@
 import React, {Component} from 'react';
 import {Row, Input,Button,Icon} from 'react-materialize';
+import {browserHistory} from 'react-router'
+import axios from 'axios';
+import Auth from './Auth.js';
 
 class Login extends Component {
+
+    constructor(){
+        super();
+    }
+
+    login(){
+
+        let credentials = {email:this.refs.email.state.value, password:this.refs.pass.state.value}
+        axios.post('http://localhost:9393/authenticate', credentials)
+            .then(function (response) {
+                debugger
+                Auth.authenticateUser(response.data.auth_token);
+                console.log(Auth.getToken());
+                browserHistory.push('/');
+            })
+            .catch(function (error) {
+                Auth.deauthenticateUser();
+                console.log(error);
+            });
+    }
+
     render() {
         return (
             <div className="center">
@@ -9,13 +33,13 @@ class Login extends Component {
                     <h3>Login</h3>
                 </Row>
                 <Row>
-                    <Input type="email" label="Email" s={12} m={12} l={12}/>
+                    <Input ref="email" type="email" label="Email" s={12} m={12} l={12}/>
                 </Row>
                 <Row>
-                    <Input type="password" label="password" s={12} m={12} l={12} />
+                    <Input ref="pass" type="password" label="password" s={12} m={12} l={12} />
                 </Row>
                 <Row>
-                    <Button waves='light'>Login</Button>
+                    <Button waves='light' onClick={this.login.bind(this)}>Login</Button>
                 </Row>
             </div>
         );
